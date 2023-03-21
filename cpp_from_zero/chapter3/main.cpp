f85dd2c7e9e841a4be78f3b1e54b856fae2781a0
mts0629 <soratobi93@gmail.com>
2023-03-21 23:59:11 +0900

    cpp_from_zero: chapter 3.9

--- cpp_from_zero/chapter3/main.cpp
+++ cpp_from_zero/chapter3/main.cpp
@@ -4,6 +4,12 @@
 #include <string> // std::string
 #include <tuple> // std::tuple
 #include <utility> // std::pair
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
 
 // Function template: get the number of elements of an array
 template <class T, std::size_t N>
@@ -124,6 +130,178 @@ int main() {
         std::string name;
         int age;
         std::tie(name, age) = person; // name = "Bob", age = 20
+    }
+
+    {
+        // vector: dynamic array
+        std::vector<int> x = { 0, 1, 2, 3, 4, 5 };
+        x[2] = 10;
+
+        // Get the size and scan elements
+        for (auto i = 0; i < x.size(); ++i) {
+            std::cout << x[i] << " ";
+        }
+        std::cout << std::endl;
+
+        // Push an element to the tail
+        x.emplace_back(5);
+        for (auto i = 0; i < x.size(); ++i) {
+            std::cout << x[i] << " ";
+        }
+        std::cout << std::endl;
+
+        // Pop an element from the tail
+        x.pop_back();
+        for (auto i = 0; i < x.size(); ++i) {
+            std::cout << x[i] << " ";
+        }
+        std::cout << std::endl;
+
+        // Pointer access
+        auto px = x.data();
+        std::cout << *px << std::endl;
+    }
+
+    {
+        // std::map: dictionary
+        std::map<std::string, int> persons = {
+            { "Alice", 18 },
+            { "Bob", 20 }
+        };
+
+        // Refer with using keys
+        std::cout << "persons[\"Alice\"] = " << persons["Alice"] << std::endl;
+        std::cout << "persons[\"Bob\"] = " << persons["Bob"] << std::endl;
+
+        // std::unordered_map
+        // Using hash for data management, and an order of data does not be saved
+        // Performance is better than that of std::map
+        std::unordered_map<std::string, int> person_unordered = {
+            { "Alice", 18 },
+            { "Bob", 20 }
+        };
+
+        // Insert an element
+        persons.insert({ "Eve", 19 });
+
+        // Remove an element by a key
+        persons.erase("Bob");
+    }
+
+    {
+        // std::set: unique data container
+        std::set<std::string> persons = {
+            "Alice",
+            "Bob"
+        };
+
+        // std::set
+        // Same with std::unordered_map, using hash
+        std::unordered_set<std::string> persons_unordrered = {
+            "Alice",
+            "Bob"
+        };
+
+        // Insert an element
+        persons.insert("Eve");
+
+        // Remove an element
+        persons.erase("Bob");
+    }
+
+    {
+        std::vector<int> x = { 0, 1, 2, 3, 4 };
+
+        // Iterator
+        auto it = x.begin();
+        // Can use like as a pointer
+        std::cout << *it << std::endl;
+        ++it;
+        std::cout << *it << std::endl;
+
+        // Loop with iterator
+        // x.end() is the next one of the last element of x
+        for (auto it = x.begin(); it != x.end(); ++it) {
+            std::cout << *it << std::endl;
+        }
+
+        // Does not depend on the type of STL containers
+        std::vector<int> y = { 0, 1, 2, 3, 4 };
+        for (auto it = y.begin(); it != y.end(); ++it) {
+            std::cout << *it << std::endl;
+        }
+
+        // Functions in <algorithm> can be used via iterator
+        auto n = std::count_if(x.begin(), x.end(), [](const int v) {
+            if (v <= 0) {
+                return false;
+            }
+            if (v %2 != 0) {
+                return false;
+            }
+            return true;
+        });
+        std::cout << n << std::endl; // 2
+
+        // An iterator for std::map, std::unordered_map are std::pair
+        std::map<std::string, int> persons = {
+            { "Alice", 18 },
+            { "Bob", 20 }
+        };
+
+        for (auto it = persons.begin(); it != persons.end(); ++it) {
+            const auto& person = *it; // std::pair<std::string, int>
+            const std::string& name = person.first;
+            const int age = person.second;
+            std::cout << name << ": " << age << std::endl;
+        }
+
+        // Forward iterator
+        std::unordered_set<int> us = { 0, 1, 2, 3, 4, 5 };
+        for (auto it = us.begin(); it != us.end(); ++it) {
+            std::cout << *it << std::endl;
+        }
+
+        // Bidirectional iterator
+        std::set<int> s = { 0, 1, 2, 3, 4, 5 };
+        for (auto it = s.begin(); it != s.end(); ++it) {
+            std::cout << *it << std::endl;
+        }
+        for (auto it = --(s.end()); it != s.begin(); --it) {
+            std::cout << *it << std::endl;
+        }
+
+        // Random access iterator
+        auto itr = x.begin();
+        itr = itr + 3;
+        std::cout << *itr << std::endl; // 3
+        std::cout << itr[-2] << std::endl; // 1
+        std::cout << *itr << std::endl; // 3, iterator was not changed
+
+        auto it1 = x.begin() + 2; // 2
+        auto it2 = x.begin() + 3; // 3
+        auto it3 = x.begin() + 4; // 4
+
+        x.erase(x.begin() + 3);
+
+        // Iterator will be invalid
+        std::cout << *it1 << std::endl; // 2
+        // std::cout << *it2 << std::endl; // Undefined behavior
+        // std::cout << *it3 << std::endl; // Undefined behavior
+    }
+
+    {
+        // Range-based for statement
+        std::vector<int> x = { 0, 1, 2, 3, 4, 5 };
+        for (auto&& e : x) {
+            std::cout << e << std::endl;
+        }
+
+        // For an array
+        int y[] = { 0, 1, 2, 3, 4, 5 };
+        for (auto&& e : y) {
+            std::cout << e << std::endl;
+        }
     }
 
     return 0;
