7c686eea021ce8192a77934994b59ed5440287dc
mts0629 <soratobi93@gmail.com>
2024-12-24 23:59:44 +0900

    Calculator: create and evaluate syntax tree

--- playground/calc/calc.cpp
+++ playground/calc/calc.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <memory>
 #include <sstream>
 #include <string>
-#include <queue>
+#include <vector>
 
 static const std::string operators = "+-*/";
 static const std::string separators = " \t" + operators;
@@ -58,78 +59,161 @@ static std::vector<std::string> tokenize(const std::string& str) {
     return tokens;
 }
 
-static bool parseNumber(double& value, const std::string& token) {
-    try {
-        auto v = stod(token);
-        value = v;
-    } catch (const std::invalid_argument e) {
-        std::cout << "Invalid token: \"" << token << "\"" << std::endl;
-        return false;
-    } catch (const std::out_of_range e) {
-        std::cout << "Value \"" << token << "\" is out of range" << std::endl;
-        return false;
+enum class ExprNodeType {
+    INVALID,
+    NUMBER,
+    ADD,
+    SUB,
+    MUL,
+    DIV
+};
+
+class ExprNode {
+public:
+    ExprNode(const double value) :
+        type_(ExprNodeType::NUMBER), value_(value), lhs_(nullptr), rhs_(nullptr) {}
+
+    ExprNode(const ExprNodeType op) :
+        type_(op), value_(0), lhs_(nullptr), rhs_(nullptr) {}
+
+    double eval() {
+        if (this->type_ == ExprNodeType::NUMBER) {
+            return this->value_;
+        }
+
+        if ((this->lhs_ == nullptr) || (this->rhs_ == nullptr)) {
+            throw std::runtime_error("Operands are lacking");
+        }
+
+        auto lhs = this->lhs_->eval();
+        auto rhs = this->rhs_->eval();
+
+        switch (this->type_) {
+            case ExprNodeType::ADD:
+                this->value_ = lhs + rhs;
+                break;
+            case ExprNodeType::SUB:
+                this->value_ = lhs - rhs;
+                break;
+            case ExprNodeType::MUL:
+                this->value_ = lhs * rhs;
+                break;
+            case ExprNodeType::DIV:
+                this->value_ = lhs / rhs;
+                break;
+            default:
+                throw std::runtime_error("Invalid operator");
+                break;
+        }
+
+        return this->value_;
     }
 
-    return true;
+    bool isNumber() {
+        return this->type_ == ExprNodeType::NUMBER;
+    }
+
+    bool isOperator() {
+        return !isNumber();
+    }
+
+    bool isLhsBlank() {
+        return this->lhs_ == nullptr;
+    }
+
+    bool isRhsBlank() {
+        return this->rhs_ == nullptr;
+    }
+
+    void addLhs(std::unique_ptr<ExprNode>& node) {
+        this->lhs_.reset();
+        this->lhs_ = std::move(node);
+    }
+
+    void addRhs(std::unique_ptr<ExprNode>& node) {
+        this->rhs_.reset();
+        this->rhs_ = std::move(node);
+    }
+
+private:
+    ExprNodeType type_;
+    double value_;
+    std::unique_ptr<ExprNode> lhs_;
+    std::unique_ptr<ExprNode> rhs_;
+};
+
+static double parseNumber(const std::string& token) {
+    try {
+        return stod(token);
+    } catch (const std::invalid_argument& e) {
+        std::cout << "Invalid token: \"" << token << "\"" << std::endl;
+        throw e;
+    } catch (const std::out_of_range& e) {
+        std::cout << "Value \"" << token << "\" is out of range" << std::endl;
+        throw e;
+    }
 }
 
-static void parseExpression(const std::vector<std::string>& tokens) {
-    std::queue<char> ops {};
-    std::queue<double> values {};
+static std::unique_ptr<ExprNode> parseToExpression(const std::vector<std::string>& tokens) {
+    std::unique_ptr<ExprNode> root {nullptr};
 
     for (auto token : tokens) {
+        std::unique_ptr<ExprNode> node {nullptr};
+
         if (isOperator(token[0])) {
-            ops.push(token[0]);
+            ExprNodeType optype = ExprNodeType::INVALID;
+            switch (token[0]) {
+                case '+':
+                    optype = ExprNodeType::ADD;
+                    break;
+                case '-':
+                    optype = ExprNodeType::SUB;
+                    break;
+                case '*':
+                    optype = ExprNodeType::MUL;
+                    break;
+                case '/':
+                    optype = ExprNodeType::DIV;
+                    break;
+                default:
+                    throw std::runtime_error("Invalid operator: " + token);
+                    break;
+            }
+            node = std::make_unique<ExprNode>(optype);
+        } else {
+            double number = parseNumber(token);
+            node = std::make_unique<ExprNode>(number);
+        }
+
+        if (root == nullptr) {
+            root = std::move(node);
             continue;
         }
 
-        double number;
-        if (parseNumber(number, token)) {
-            values.push(number);
-            continue;
+        if (node->isOperator()) {
+            node->addLhs(root);
+            root.reset();
+            root = std::move(node);
+        } else {
+            if (root->isLhsBlank()) {
+                root->addLhs(node);
+            } else if (root->isRhsBlank()) {
+                root->addRhs(node);
+            } else {
+                throw std::runtime_error(
+                    "No corresponding operators to " + \
+                    std::to_string(node->eval())
+                );
+            }
         }
     }
 
-    if ((ops.size() != 1) || (values.size() != 2)) {
-        std::cout << "Format: VALUE1 ['+'|'-'|'*'|'/'] VALUE2" << std::endl;
-        return;
-    }
-
-    double result = 0.0;
-
-    auto op = ops.front();
-    ops.pop();
-    auto v1 = values.front();
-    values.pop();
-    auto v2 = values.front();
-    values.pop();
-
-    switch (op) {
-        case '+':
-            result += v1 + v2;
-            break;
-        case '-':
-            result += v1 - v2;
-            break;
-        case '*':
-            result += v1 * v2;
-            break;
-        case '/':
-            result += v1 / v2;
-            break;
-        default:
-            std::cout << "Invalid operator: \'" << op << "\'" << std::endl;
-            return;
-            break;
-    }
-
-    std::cout << result << std::endl;
+    return root;
 }
 
 int main() {
     while (true) {
         std::cout << "> ";
-
         std::string user_input {};
         std::getline(std::cin, user_input);
 
@@ -137,8 +221,13 @@ int main() {
             break;
         }
 
-        auto tokens = tokenize(user_input);
-        parseExpression(tokens);
+        try {
+            auto tokens = tokenize(user_input);
+            auto expr = parseToExpression(tokens);
+            std::cout << expr->eval() << std::endl;
+        } catch (const std::exception& e) {
+            (void)e;
+        }
     }
 
     return 0;
